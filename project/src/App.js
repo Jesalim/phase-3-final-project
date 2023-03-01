d96fe1545069829f4367d772f913e2e86c94d20c
Jesalim <jessyculture@gmail.com>
2023-03-01 17:23:43 +0300

    app.js update

--- project/src/App.js
+++ project/src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+// Routes
+import { Routes, Route, Navigate } from 'react-router-dom';
+
+// Hooks
+import { useEffect, useState } from 'react';
+import { useAuth } from './hooks/useAuth';
+
+// Components
+import Header from './components/Header';
+
+// Pages
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Details from './pages/Details';
+import Profile from './pages/Profile';
+import NewReview from './pages/NewReview';
+import Loading from './components/Loading';
+import Favorites from './pages/Favorites';
 
 function App() {
+  const [user, setUser] = useState(undefined);
+
+  const { auth, onAuthStateChanged } = useAuth();
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setUser(user);
+    });
+  }, [auth, onAuthStateChanged]);
+
+  if (user === undefined) {
+    return (
+      <div className='flex items-center justify-center sectionHeight'>
+        <Loading size={'60px'} />
+      </div>
+    );
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Header user={user} />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={user ? <Navigate to='/' /> : <Login />} />
+        <Route
+          path='/register'
+          element={user ? <Navigate to='/' /> : <Register />}
+        />
+        <Route path='/details/:id' element={<Details user={user} />} />
+        <Route
+          path='/profile'
+          element={!user ? <Navigate to='/' /> : <Profile user={user} />}
+        />
+        <Route
+          path='/favorites'
+          element={!user ? <Navigate to='/' /> : <Favorites user={user} />}
+        />
+        <Route
+          path='/add-review'
+          element={!user ? <Navigate to='/' /> : <NewReview user={user} />}
+        />
+        <Route path='/details/:id' element={<Details />} />
+        <Route path='/*' element={<Navigate to='/' />} />
+      </Routes>
     </div>
   );
 }
